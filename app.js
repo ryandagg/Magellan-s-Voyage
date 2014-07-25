31ed1e03a8ce582e2ca34c2533110b6d9d9252f1
gbachik <gbachik@gmail.com>
2014-07-25 12:44:35 -0600

    Finished Project and Bonus

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+var cheerio = require('cheerio');
+var request = require('request');
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -5,10 +7,50 @@ var app = express();
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser());
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/', function(req, res) {
-	res.render('index');
+app.get('/:destination?', function(req, res) {
+	console.log(req.params);
+	var dest = req.params.destination || 'seville';
+	if (dest === 'next') {
+
+		url = 'http://localhost:5893/' + req.query.location;
+
+	    // The structure of our request call
+	    // The first parameter is our URL
+	    // The callback function takes 3 parameters, an error, response status code and the html
+
+		request(url, function(error, response, html){
+
+	        // First we'll check to make sure no errors occurred when making the request
+
+	        if(!error){
+	            // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
+	            var $ = cheerio.load(html);
+
+
+	            var currentNext = {
+	            	location: req.query.location,
+	            	nextLocation: $('a').attr('href').split('/').join('')
+	            }
+	            res.send(JSON.stringify(currentNext));
+			}
+		});
+
+	}else if (dest === 'favicon.ico') { 
+	}else{
+		res.render(dest.toLowerCase(), {
+			dest: dest
+		}, function(err, html){
+			if (err) {
+				res.render('error', {
+					dest: dest
+				});
+			}else{
+				res.end(html);
+			}
+		});
+	}
 });
 
 var server = app.listen(5893, function() {
